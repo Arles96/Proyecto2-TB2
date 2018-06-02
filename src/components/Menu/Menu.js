3ecbabf5eb9473ce0c88012b4e4c88c453f6ac8f
agomezomg <anagomez98@unitec.edu>
2018-06-02 12:26:55 -0600

    menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -3,28 +3,22 @@ export default class Menu extends Component {
     render() {
         return(
             <div>
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#">Disabled</a>
-                    </li>
-                    </ul>
-                </div>
-                </nav>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light row">
+                    <div className = "col-6 text-left">
+
+                        <a className="navbar-brand" href="#">Centro de Empleo</a>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                    </div>
+                    <div className="collapse navbar-collapse col-6 text-right" id="navbarSupportedContent">
+                        <form className="form-inline my-2 my-lg-0">
+                            <input className="form-control mr-sm-2" type="number" placeholder="CIF" aria-label="Search"/>
+                            <input className="form-control mr-sm-2" type="password" placeholder="Clave" aria-label="Search"/>
+                            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button>
+                        </form>
+                    </div>
+                </nav>                
             </div>
         )
     }
