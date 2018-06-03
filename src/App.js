18de17f787d721e8a608b6aa02174200aec5daa2
Arles96 <arles.cerrato@unitec.edu>
2018-06-03 10:22:03 -0600

    Agregando SubMenu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Menu from './components/Menu/Menu'
+import SubMenu from './components/SubMenu/SubMenu'
 
 class App extends Component {
 
   constructor(){
     super()
+    this.state = {
+      submenu : false,
+      login : false
+    }
     this.menuExtend=this.menuExtend.bind(this)
+    this.closeMenu = this.closeMenu.bind(this)
+    this.renderMenu = this.renderMenu.bind(this)
   }
 
   menuExtend(){
-    console.log("boo")
+    this.setState({
+      submenu : true
+    })
+  }
+
+  closeMenu(){
+    this.setState({
+      submenu : false
+    })
+  }
+
+  renderMenu(){
+    if (this.state.submenu){
+      return (
+        <SubMenu login={this.state.login} event={this.closeMenu} />
+      )
+    }
   }
 
   render() {
     return (
       <div className="App">
           <Menu event={this.menuExtend} />
+          {this.renderMenu()}
       </div>
     );
   }
---
+++ src/components/SubMenu/SubMenu.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react'
+import './SubMenu.css'
+
+export default class SubMenu extends Component {
+
+    constructor(){
+        super()
+        this.renderButton = this.renderButton.bind(this)
+    }
+
+    renderButton(){
+        if (this.props.login){
+            return (
+                <div>
+                    <li className="nav-item link-menu" >
+                        <a href="#Home" className="nav-link">Enviar Solicutud</a>
+                    </li>
+                    <hr className="hr" />               
+                    <li className="nav-item link-menu" >
+                        <a href="#Home" className="nav-link">Registrar Empresa</a>
+                    </li>
+                    <hr className="hr" />  
+                </div>
+            )
+        }else {
+            return (
+                <div>
+                    <li className="nav-item link-menu" >
+                        <a href="#Home" className="nav-link">Crear Puesto</a>
+                    </li>
+                    <hr className="hr" />               
+                    <li className="nav-item link-menu" >
+                        <a href="#Home" className="nav-link">Solicitudes</a>
+                    </li>
+                    <hr className="hr" />  
+                </div>
+            )
+        }
+    }
+
+    render(){
+        return (
+            <nav className="animated bounceInLeft menu-deploy bg-light" >
+                <div className="text-right menu-top" >
+                    <i onClick={this.props.event} className="menu-button fas fa-times-circle"></i>
+                    </div>
+                <ul className="nav flex-column nav-color2">
+                    <li className="nav-item link-menu" >
+                        <a href="#Home" className="nav-link">Home</a>
+                    </li>
+                    <hr className="hr" />
+                    <li className="nav-item link-menu" >
+                        <a href="#Home" className="nav-link">Empresas</a>
+                    </li>
+                    <hr className="hr" /> 
+                    {this.renderButton()}
+                </ul>
+            </nav>
+        )
+    }
+
+}
